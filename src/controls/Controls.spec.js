720c1afadd0ba007ca3b4deb610aa750efbd4883
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-08-19 17:03:49 -0500

    WIP: Added imports to spec.js files.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,9 @@
 // Test away!
+
+// react imports 
+import React from "react";
+import renderer from "react-test-renderer"; 
+import { render, fireEvent } from "@testing-library/react";
+
+// components 
+import Controls from "./Controls.js"; 
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,9 @@
 // Test away!
+
+// react imports 
+import React from "react";
+import renderer from "react-test-renderer"; 
+import { render, fireEvent } from "@testing-library/react";
+
+// components 
+import Display from "./Display.js"; 
