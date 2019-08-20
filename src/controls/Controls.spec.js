e5a9cd2c60ccf2f8a01fec26c5d35375b7a1c5f0
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-08-20 09:14:12 -0500

    MVP Complete.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -2,7 +2,8 @@
 
 // react imports 
 import React from "react";
-import { render } from "@testing-library/react";
+import renderer from "react-test-renderer"; 
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";  
 
 // components 
@@ -18,7 +19,6 @@ describe("<Controls />", () => {
         // intial render should have buttons with the text Lock Gate and Close Gate 
         expect(getByTestId("un-lock-gate-btn")).toHaveTextContent(/Lock Gate/i);
         expect(getByTestId("open-close-gate-btn")).toHaveTextContent(/Close Gate/i);
-
     });
 
     it("controls cannot be closed or opened if it is locked", () => {
@@ -28,5 +28,38 @@ describe("<Controls />", () => {
         
         // the open/close button should be diabled now 
         expect(getByTestId("open-close-gate-btn")).toBeDisabled();
-    })
+    }); 
+
+    it("provide buttons to toggle the `closed` and `locked` states", () => { 
+
+        // generates a DOM tree
+        const controlTree = renderer.create(<Controls />); 
+
+        // snapshots are a JSON representation of the DOM tree
+        expect(controlTree.toJSON()).toMatchSnapshot();
+    });
+
+    it("buttons' text changes to reflect the state the door will be in if clicked", () => {
+        const { getByTestId } = render(<Controls locked={true}/>);
+
+        expect(getByTestId("un-lock-gate-btn")).toHaveTextContent("Unlock Gate")
+
+        expect(getByTestId("open-close-gate-btn")).toHaveTextContent("Close Gate")
+
+        expect(getByTestId("open-close-gate-btn")).toBeDisabled();
+
+    }); 
+
+    it("the closed toggle button is disabled if the gate is locked", () => {
+        const { getByTestId } = render(<Controls locked={true}/>);
+
+        expect(getByTestId("open-close-gate-btn")).toBeDisabled();
+    }); 
+
+    it("the locked toggle button is disabled if the gate is open", () => {
+        const { getByTestId } = render(<Controls closed={false}/>);
+
+        expect(getByTestId("un-lock-gate-btn")).toBeDisabled();
+    }); 
+
 });
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,7 +3,6 @@
 // react imports 
 import React from "react";
 import renderer from "react-test-renderer"; 
-import { render, fireEvent } from "@testing-library/react";
 
 // components 
 import DashBoard from "./Dashboard.js"; 
